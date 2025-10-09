36165f8c6d124361f92c04021cc5bb27617c811e
Wahyu <wahyupisang6@gmail.com>
2025-10-09 09:02:15 +0700

    Add files via upload

---
+++ pertemuan4/ngulang/routing_app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        nama = request.form['nama']
+        alamat = request.form['alamat']
+        return f"Hallo, {nama} dari {alamat}. Form berhasil dikirim!"
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
